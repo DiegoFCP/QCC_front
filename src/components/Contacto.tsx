2bfd7ce2f9d2da370e96ecdd9a8c7e4f92f2c5a5
DiegoFCP <diegocortes.pinilla@gmail.com>
2025-06-01 20:04:29 -0400

    añade seccion Contacto

---
+++ src/components/Contacto.tsx
@@ -0,0 +1,14 @@
+
+import "./Contacto.css"
+
+
+function Contacto() {
+    return (
+        <div>
+            <h2>Contacto</h2>
+            <p>Puedes contactarnos al correo contacto@ejemplo.com</p>
+        </div>
+);
+}
+
+export default Contacto;
